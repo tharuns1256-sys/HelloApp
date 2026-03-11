c48e9194ee1548d2f3115159621dab7461135bf6
tharuns1256-sys <tharuns1256@gmail.com>
2026-03-11 15:21:13 +0530

    UC2 Done

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,11 @@
-public class HelloApp {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+public class Hello {
+  public static void main(String[] args) {
+     if (args.length > 0) {
+      String name = args[0];
+      System.out.println("Hello " + name + "!");
+     }
+     else {
+      System.out.println("Hello World!");
+     }
+  }
 }

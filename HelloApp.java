f61d51c909bfb77fe317414005941966fd91479f
tharuns1256-sys <tharuns1256@gmail.com>
2026-03-26 13:42:29 +0530

    Refactor Hello class to HelloApp with improved logic

--- HelloApp.java
+++ HelloApp.java
@@ -1,11 +1,16 @@
-public class Hello {
-  public static void main(String[] args) {
-     if (args.length > 0) {
-      String name = args[0];
-      System.out.println("Hello " + name + "!");
-     }
-     else {
-      System.out.println("Hello World!");
-     }
-  }
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
+
 }
